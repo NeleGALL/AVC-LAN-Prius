1f054767518d8bdf722c1cc1dec535df535909e1
NeleGALL <ru+gh@burubu.ru>
2015-01-22 10:48:57 +1000

    V 0.9
    Main

---
+++ Main/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(220, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 39);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(498, 428);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(490, 402);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Настройки";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(490, 402);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Консоль";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(481, 393);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 479);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Main/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using CoreAudioApi;
+using CoreAudioApi.Interfaces;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        static SerialPort sp = new SerialPort();
+        Thread readThread = new Thread(Read);
+        static MMDeviceEnumerator devEnum;
+        static MMDevice defaultDevice;
+
+        string COM;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static public void ReInitVolume()
+        {
+            devEnum = new MMDeviceEnumerator();
+            defaultDevice = devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+        }
+        static public Boolean Reconnect()
+        {
+            while(true)
+            {
+                try
+                {
+                    sp.Open();
+                    return true;
+                }
+                catch{}
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            if(Properties.Settings.Default.Port == string.Empty)
+            {
+                comboBox1.Items.AddRange(SerialPort.GetPortNames());
+            }
+            COM = Properties.Settings.Default.Port;
+            if(COM != "" && comboBox1.Items.Contains(COM) == false){comboBox1.Items.Add(COM);}
+            if(comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+                button2.Enabled = true;
+            }
+            ReInitVolume();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sp.PortName = comboBox1.SelectedItem.ToString();
+                sp.BaudRate = 250000;
+                sp.ReadTimeout = 500;
+                Reconnect();
+                readThread.Start();
+                button2.Enabled = false;
+            }
+            catch { }
+        }
+        public static void Read()
+        {
+            while (true)
+            {
+                try
+                {
+                    string message = sp.ReadLine();
+                    CheckAndDo(message);
+                }
+                catch (TimeoutException) { }
+                catch (Exception) 
+                {
+                    if (!sp.IsOpen)
+                    {
+                        try { Reconnect(); }
+                        catch { }
+                    }
+                }
+            }
+        }
+        static public Boolean CheckAndDo(string str)
+        {
+            if (str.Contains("0F7431F110"))
+            {
+                int where = str.IndexOf("0F7431F110");
+                string val = str.Remove(0, where + "0F7431F110".Length);
+                val = val.Remove(2);
+                SetVolume(Convert.ToInt32(val), 51);
+
+            }
+            else if (str.Contains("0C0025238408"))
+            {
+                SendKey("{F7}");
+            }
+            else if (str.Contains("0C0025238404"))
+            {
+                SendKey("{F6}");
+            }
+            else if (str.Contains("05002532801D"))
+            {
+                SendKey("{F8}");
+            }
+            return false;
+        }
+
+        #region Does
+        static public void SetVolume(int val, int max)
+        {
+            ReInitVolume();
+            float newval = (float)val / (float)max;
+            defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = newval;
+        }
+        static public void SendKey(string keys)
+        {
+            SendKeys.SendWait(keys);
+        }
+
+        static public void MouseSet(int x, int y)
+        {
+            uint dx = (uint)x;
+            uint dy = (uint)y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, dx, dy, 0, 0);
+        }
+        #endregion
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sp.Close();
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+}
